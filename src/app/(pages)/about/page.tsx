68bbf53bbface99f4154cf5c584aaf9f4702c5b9
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2025-06-05 18:19:12 -0400

    feat: about top section

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,5 +1,34 @@
-const page = () => {
-  return <div>page</div>;
+import WordAnimation from "@/components/WordAnimation/WordAnimation";
+import styles from "./styles.module.scss";
+import ImageComp from "@/components/ImageComp/ImageComp";
+
+const About = () => {
+  return (
+    <main className={styles["main"]}>
+      <WordAnimation
+        text="WHO IS THIS GUY?"
+        element="h1"
+        className={styles["title"]}
+      />
+
+      <section className={styles["content"]}>
+        <div className={styles["text"]}>
+          <WordAnimation
+            text="Lorem ipsum dolor sit amet consectetur. Eu nibh maecenas ac sed leo dolor. Vel sit eget laoreet arcu neque. Suspendisse massa aliquet nisi at. Blandit vestibulum suspendisse duis ut ut accumsan eget condimentum."
+            element="p"
+            className={styles["paragraph"]}
+            letterDelay={0.03}
+            duration={1}
+          />
+        </div>
+        <div className={styles["image"]}>
+          <div className={styles["image-container"]}>
+            <ImageComp alt="About Image" img="/images/me.png" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
-export default page;
+export default About;
---
+++ src/components/ImageComp/ImageComp.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export default function ImageComp({
+  img = "/default-image.jpg",
+  alt = "Parallax image",
+}) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], ["0%", "-10%"]);
+
+  return (
+    <motion.div
+      className={styles["image-container"]}
+      ref={ref}
+      initial={{ filter: "blur(30px)", opacity: 0, y: "10%" }}
+      animate={{ filter: "blur(0px)", opacity: 1, y: "0%" }}
+      transition={{ duration: 1.4, ease: [0.56, 0, 0, 1] }}
+    >
+      <motion.div style={{ translateY: translateY }}>
+        <Image
+          src={img}
+          alt={alt}
+          fill
+          style={{ objectFit: "cover" }}
+          priority={false}
+          quality={85}
+        />
+      </motion.div>
+    </motion.div>
+  );
+}
