bdc96c18393b3db072a76a479d39145b0655f4f3
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2025-06-07 03:55:08 -0400

    feat: section 2 not responsive yet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import LandingSection from "@/components/sections/Home/LandingSection/LandingSection";
-import "./globals.scss";
+import DescSection from "@/components/sections/Home/DescSection/DescSection";
 import InfoSection from "@/components/sections/Home/InfoSection/InfoSection";
+import "./globals.scss";
 
 export default function Home() {
   return (
     <>
       <LandingSection />
       <InfoSection />
+      <DescSection />
     </>
   );
 }
---
+++ src/components/sections/Home/DescSection/DescSection.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import styles from "./styles.module.scss";
+
+const DescSection = () => {
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  const letterRefs = useRef<(HTMLSpanElement | null)[]>([]);
+  const containerRef = useRef<HTMLElement>(null);
+
+  const splitLetters = (word: string) => {
+    const letters: React.ReactNode[] = [];
+    word.split("").forEach((letter, index) => {
+      letters.push(
+        <span
+          key={letter + "_" + index}
+          ref={(el) => {
+            letterRefs.current.push(el);
+          }}
+          className={styles["letter"]}
+        >
+          {letter}
+        </span>
+      );
+    });
+    return letters;
+  };
+
+  const splitWords = (text: string) => {
+    const words: React.ReactNode[] = [];
+
+    text.split(" ").forEach((word, index) => {
+      words.push(
+        <span key={word + "_" + index} className={styles["word"]}>
+          {splitLetters(word)}
+        </span>
+      );
+    });
+    return words;
+  };
+
+  const createAnimation = () => {
+    gsap.to(letterRefs.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        scrub: true,
+        start: "top-=50% top",
+        end: `+=${window.innerHeight / 1.6}`,
+        markers: true,
+      },
+      opacity: 1,
+      ease: "none",
+      stagger: 0.1,
+    });
+  };
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    createAnimation();
+  });
+
+  return (
+    <section className={styles["desc"]} ref={containerRef}>
+      <div className={styles["desc__container"]}>{splitWords(text)}</div>
+    </section>
+  );
+};
+
+export default DescSection;
