2b28bb1e3fe5fb10d364ebe9b5586cab114089e5
Haneesh Raj <haneeshraj.banisetti@gmail.com>
2025-06-09 17:29:01 -0400

    feat: footer structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import LetterAnimation from "../LetterAnimation/LetterAnimation";
+
 import styles from "./styles.module.scss";
 
 const Footer = () => {
-  return <div className={styles["footer-container"]}></div>;
+  return (
+    <footer className={styles["footer"]}>
+      <div className={styles["footer__logo"]}></div>
+      <div className={styles["footer__contact"]}></div>
+      <div className={styles["footer__nav"]}></div>
+      <div className={styles["footer__socials"]}></div>
+      <div className={styles["footer__copyright"]}></div>
+    </footer>
+  );
 };
 
 export default Footer;
